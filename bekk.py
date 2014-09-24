2140f3dd55887c0686758fb77677448ffbccc9cb
khrapovs <khrapovs@gmail.com>
2014-09-24 15:55:28 +0400

    Module header

--- bekk.py
+++ bekk.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This module allows to simulate and estimate the BEKK(1,1) model.
+
+Robert F. Engle and Kenneth F. Kroner
+"Multivariate Simultaneous Generalized Arch"
+Econometric Theory, Vol. 11, No. 1 (Mar., 1995), pp. 122-150
+http://www.jstor.org/stable/3532933
+
+"""
+
 # Check this repo for related R library: https://github.com/vst/mgarch/
 # Alternative optimization library: http://www.pyopt.org/
 
@@ -8,12 +19,11 @@ import scipy as sp
 import matplotlib.pylab as plt
 from scipy.optimize import minimize
 import scipy.linalg as sl
-#import cProfile
-#import numba as nb
 import time
-#from IPython.parallel import Client
 
-np.set_printoptions(precision=4, suppress=True)
+__author__ = "Stanislav Khrapov, Stanislav Anatolyev"
+__email__ = "khrapovs@gmail.com"
+__status__ = "Development"
 
 class BEKK(object):
     """BEKK model. Estimation class.
