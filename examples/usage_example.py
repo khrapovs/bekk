081eb0048935b93213679aec08429ac62889376a
Stanislav Khrapov <khrapovs@gmail.com>
2015-03-24 18:07:23 +0300

    Return to Powell optimizer

--- examples/usage_example.py
+++ examples/usage_example.py
@@ -72,7 +72,7 @@ def test_bekk(nstocks=2, nobs=500, restriction='scalar', var_target=True,
     time_start = time.time()
     bekk.estimate(param_start=param_true, param_true=param_true,
                   restriction=restriction, var_target=var_target,
-                  method='SLSQP', log_file=log_file, parallel=False)
+                  method='Powell', log_file=log_file, parallel=False)
     print('Time elapsed %.2f, seconds' % (time.time() - time_start))
     bekk.print_error()
 
@@ -81,7 +81,7 @@ def test_bekk(nstocks=2, nobs=500, restriction='scalar', var_target=True,
 
 if __name__ == '__main__':
     np.set_printoptions(precision=4, suppress=True)
-    nstocks = 1
+    nstocks = 2
     var_target = False
     nobs = 500
     bekk = test_bekk(nstocks=nstocks, simulate=True, var_target=var_target,
