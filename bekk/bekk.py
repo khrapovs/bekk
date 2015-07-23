a0c29271b9d996e5a9b8e6ca5f957c41c293921f
Stanislav Khrapov <khrapovs@gmail.com>
2015-07-23 16:58:05 +0300

    Specify package directory

--- bekk/bekk.py
+++ bekk/bekk.py
@@ -16,8 +16,8 @@ from scipy.optimize import minimize
 
 from .bekkparams import BEKKParams
 from .utils import estimate_h0, likelihood_python, filter_var_python
-from bekk.recursion import recursion
-from bekk.likelihood import likelihood
+from .recursion import recursion
+from .likelihood import likelihood
 
 __all__ = ['BEKK']
 
